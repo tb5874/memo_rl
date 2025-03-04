50f7c813f6ca173ced5af3352653182833a9b90d
taebin.heo <taebin.heo@konantech.com>
2025-03-04 18:02:55 +0900

    [Modify] power bandit !!!

--- python_src/rl_bandit.py
+++ python_src/rl_bandit.py
@@ -2,11 +2,20 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
+def util_top(input, top):
+    np.set_printoptions(suppress=True)
+    np.set_printoptions(linewidth=1000)
+    top_idx = np.argsort(input)[-10:][::-1]
+    top_range = (top if len(input) > top else len(input))
+    for idx in range(0, top_range) :
+        print("top(", idx, ") : ", f'{top_idx[idx]:5}', " : ", input[top_idx[idx]], "\n", end ="" )
+    np.set_printoptions()
+    return
 
-class Bandit:    
+class Bandit :
     def __init__(self, arms):
-        np.random.seed(1234) # 생성된 Bandit 머신의 확률을 매 실험마다 고정시키기 위한 트릭             
-        self.arms = arms # arms = 슬롯머신 수
+        np.random.seed(1234) # 생성하는 Bandit 머신의 확률을 매 실험마다 고정시키기 위한 트릭
+        self.arms = arms # 슬롯머신 수
         self.rates = np.random.rand( self.arms )
             # 0 ~ 1 사이의 랜덤값을 슬롯머신 수 만큼 생성
             # 각 랜덤값은 슬롯머신의 패배 확률
@@ -14,179 +23,147 @@ class Bandit:
 
     def play(self, arm):
         rate = self.rates[arm]
+        #self.rates += 0.1 * np.random.randn(self.arms)
         return (1 if rate > np.random.rand() else 0)
 
-    def get_rate(self, arm = -1, print_flag = True):
-        if (arm == -1):
-            np.set_printoptions(suppress=True)
-            np.set_printoptions(linewidth=1000)
-            for idx in range(0, len(self.rates), 10):
-                print(self.rates[idx:idx+10])
-            np.set_printoptions()
-            print( "" )
-        else:
-            if ( print_flag == True ):
-                print("bandit    : ", arm, "\n", end ="" )
-                print("bandit Q  : ", self.rates[arm], "\n", end ="" )
+    def rate_all(self):
         return self.rates
 
-    def top_rate(self, top):
-        np.set_printoptions(suppress=True)
-        np.set_printoptions(linewidth=1000)
-        top_idx = np.argsort(self.rates)[-10:][::-1]
-        top_range = (top if len(self.rates) > top else len(self.rates))
-        for idx in range(0, top_range) :
-            print("bandit top(", idx, ") : ", f'{top_idx[idx]:5}', " : ", self.rates[top_idx[idx]])
-        np.set_printoptions()
+    def rate_arm(self, arm, flag_show = True):
+        if ( flag_show == True ):
+            print("bandit -", arm, ":", self.rates[arm], "\n", end ="" )
+        return self.rates[arm]
 
-class Agent:
-    # 정보 저장용 변수
+class Agent :
     def __init__(self, arms, epsilon):
-        self.arms = arms
+        self.arms = arms # 평가 할 슬롯머신 수
         self.info = np.zeros((2, self.arms))
-            # index 0 : 각 슬롯머신의 누적 플레이 횟수
-            # index 1 : 각 슬롯머신의 현재 Q(가치, 기대값)
-        self.epsilon = epsilon
-            # 무작위로 행동할 확률(탐색 확률)
+            # index 0 행 : 각 슬롯머신의 누적 플레이 횟수
+            # index 1 행 : 각 슬롯머신의 가치, 기대값 ( Q )
+        self.epsilon = epsilon # 무작위로 행동할 확률 ( ε )
 
     def update(self, arm, reward):
-        arm_try = self.info[0,:]
-        arm_Q = self.info[1,:]
-        arm_try[arm] += 1
-        arm_Q[arm] += (reward - arm_Q[arm]) / arm_try[arm]
+        arms_TRY = self.info[0,:]
+        arms_Q = self.info[1,:]
+        arms_TRY[arm] += 1
+        arms_Q[arm] += (reward - arms_Q[arm]) / arms_TRY[arm]
+        return
 
-    def get_action(self):
-
-        np.random.seed( int(time.perf_counter() * 1e9) % (2**32) ) # 행동 선택 시, 랜덤 요소를 정말 랜덤하게 가져가기 위함
+    def get_ACT(self):
 
         case = 0 # [ ε- greedy ]
         case = 1 # [ UCB, Upper Confidence Bound ]
-        case = 2 # [ UCB + ε- greedy ]
+
+        arms_TRY = self.info[0,:]
+        arms_Q = self.info[1,:]
 
         if (case == 0):
             # [ ε- greedy ]
-            arm_Q = self.info[1,:]
-            if np.random.rand() < self.epsilon:
-                return np.random.randint(0, len(arm_Q) ) # 무작위 행동
-            else:                
-                return np.argmax( arm_Q ) # 가장 큰 기대값을 선택
-        elif(case == 1):
+            np.random.seed( int(time.perf_counter() * 1e9) % (2**32) ) # 매 실험마다 다른 결과를 만들기 위함
+            if (self.epsilon > np.random.rand()) : return np.random.randint(0, len(arms_Q)) # 무작위 행동
+            else : return np.argmax(arms_Q) # 가장 큰 기대값을 선택
+        elif (case == 1):
             # [ UCB ]
-            arm_try = self.info[0,:]
-            arm_Q = self.info[1,:]
-            total_try = sum(arm_try)
-            if 0 in arm_try:
-                return np.argmax( arm_try == 0 ) # 모든 슬롯머신이 최소 1번씩은 수행할 수 있도록 트릭을 준다.
+            if 0 in arms_TRY:
+                return np.argmax(arms_TRY == 0) # 모든 슬롯머신이 최소 1번 수행하도록 트릭을 준다.
             else:
-                ucb_values = arm_Q + np.sqrt( (2 * np.log( total_try )) / arm_try  ) # 모든 슬롯머신의 UCB 값을 구한다.
-                return np.argmax( ucb_values ) # 가장 큰 UCB값을 선택
-        elif(case == 2):
-            # [ UCB + ε- greedy ]
-            arm_try = self.info[0,:]
-            arm_Q = self.info[1,:]
-            total_try = sum(arm_try)
-            if np.random.rand() < self.epsilon:
-                return np.random.randint(0, len(arm_Q) ) # 무작위 행동
-            else:                
-                if 0 in arm_try:
-                    return np.argmax( arm_try == 0 ) # 모든 슬롯머신이 최소 1번씩은 수행할 수 있도록 트릭을 준다.
-                else:
-                    ucb_values = arm_Q + np.sqrt( (2 * np.log( total_try )) / arm_try  ) # 모든 슬롯머신의 UCB 값을 구한다.
-                    return np.argmax( ucb_values ) # 가장 큰 UCB값을 선택
+                try_total = sum(arms_TRY)
+                arms_ucb = arms_Q + np.sqrt( ( 2 * np.log(try_total) ) / arms_TRY  ) # 모든 슬롯머신의 UCB 값을 구한다.
+                return np.argmax( arms_ucb ) # 가장 큰 UCB 값 선택
         else:
             print("not yet")
 
-    def get_maxQ(self, prnit_flag = True):
-        idx = np.argmax( self.info[1] )
-        if( prnit_flag == True):            
-            print( "agent     : ", idx, "(", int(self.info[0][ idx ]),"try )\n", end ="" )
-            print( "agent Q   : ", self.info[1][ idx ], "\n", end ="" )
-        return idx
+        return
 
-    def get_Q(self, prnit_flag = True):
-        target = self.info[1]
-        if ( prnit_flag == True ):
-            np.set_printoptions(suppress=True)
-            np.set_printoptions(linewidth=1000)            
-            for idx in range(0, len(target), 10):
-                print(target[idx:idx+10])
-            np.set_printoptions()
-        return target
+    def get_Q(self):
+        return self.info[1]
 
-def top_rate(title, input, top):
-    np.set_printoptions(suppress=True)
-    np.set_printoptions(linewidth=1000)
-    top_idx = np.argsort(input)[-10:][::-1]
-    top_range = (top if len(input) > top else len(input))
-    for idx in range(0, top_range) :
-        print(title, "top(", idx, ") : ", f'{top_idx[idx]:5}', " : ", input[top_idx[idx]], "\n", end ="" )
-    np.set_printoptions()
+    def get_maxQ(self, flag_show = True):
+        maxQ = np.argmax( self.info[1] )
+        if( flag_show == True ):
+            print( "agent -", maxQ, "(", int(self.info[0][ maxQ ]),"try ) :",self.info[1][ maxQ ] ,"\n", end ="" )
+        return maxQ
 
-# Unit Test 의 그래프는 경향성을 보기 위해서 필요하다.
+# Test 01
+# 1회 반복의 그래프는 경향성을 보기 위해 필요하다.
 def Test01():
 
     arms = 100
     steps = 10000
 
     bandit = Bandit(arms)
-    agent = Agent(arms, 0.1)
-    reward_sum = 0 # 누적 보상
-    reward_total = [] # 누적 단계별 보상
-    rates = [] # 누적 단계별 승률
-    act_arm = [] # 누적 단계별 행동
+    agent = Agent(arms, 0.1)    
+
+    ACT_step = [] # 단계별 선택 행동
+    R_total = 0 # 누적 보상
+    R_step = [] # 모든 슬롯머신을 사용하며 누적된 보상
+    Q_step = [] # 모든 슬롯머신을 사용하며 얻은 기대값
 
     for step in range(steps):
-        action = agent.get_action() # 행동 선택
-        reward = bandit.play(action) # 실제로 플레이하고 보상을 받음
-        agent.update(action, reward) # 행동과 보상을 통해 각 슬롯머신의 기대치를 갱신 == 학습
-        reward_sum += reward # 현재까지 이긴 횟수
-        rates.append( reward_sum / (step + 1) ) # 누적 플레이 마다 승률(여기서는 기대값)
-        reward_total.append(reward_sum)         # 누적 플레이 마다 이긴 횟수 저장
-        act_arm.append(action)                  # 누적 플레이 마다 선택한 행동
+        ACT = agent.get_ACT() # 정책( 기대값, 반복 횟수, 누적 행동 선택 횟수, 무작위 등... )을 기반으로 행동 선택
+        reward = bandit.play(ACT) # 선택한 행동에 대한 보상
+        agent.update(ACT, reward) # 보상으로 선택한 행동의 기대값을 갱신 == 학습
+
+        ACT_step.append(ACT)
+        R_total += reward
+        R_step.append(R_total)
+        Q_step.append(R_total / (step + 1))
 
     print("total try : ", steps, "\n", end = "")
-    arm_maxQ = agent.get_maxQ()
-    top_rate("bandit", bandit.get_rate(arm_maxQ), 10)
+    agent_maxQ = agent.get_maxQ()
+    bandit_Q = bandit.rate_arm(agent_maxQ)
 
-    plt.ylabel('Total reward')
+    print("[ bandit ]\n", end = "")
+    bandit_allQ = bandit.rate_all()
+    util_top(bandit_allQ, 10)
+
+    print("[ agent ]\n", end = "")
+    agent_allQ = agent.get_Q()
+    util_top(agent_allQ, 10)
+
+    plt.ylabel('Reward')
     plt.xlabel('Steps')
-    plt.plot(reward_total)
+    plt.plot(R_step)
     plt.show()
 
-    plt.ylabel('Rates')
+    plt.ylabel('Q')
     plt.xlabel('Steps')
-    plt.plot(rates)
+    plt.plot(Q_step)
     plt.show()
 
     plt.ylabel('Action')
     plt.xlabel('Steps')
-    plt.scatter(range(len(act_arm)), act_arm, s=1)
+    plt.scatter(range( len(ACT_step) ), ACT_step, s=1)
     plt.show()
 
     return
 
-# Batch Test 의 그래프는 의미가 없다 ?
+# Test 02
+# n 회 반복의 그래프는 무슨 의미 ?
+# 편차 ?
 def Test02(batch):    
 
     arms = 100
     steps = 10000
 
     bandit = Bandit(arms)
+    agent_Q = []
 
-    arm_batchQ = []
-
-    for batch_setp in range(batch):
+    # need to Parallelization : -->
+    for batch_step in range(batch):
         agent = Agent(arms, 0.1)
         for step in range(steps):
-            action = agent.get_action() # 행동 선택
-            reward = bandit.play(action) # 실제로 플레이하고 보상을 받음
-            agent.update(action, reward) # 행동과 보상을 통해 각 슬롯머신의 기대치를 갱신 == 학습
-        arm_batchQ.append( agent.get_Q(False) )
-        print(f"\rbatch : {batch_setp}", end="")    
+            ACT = agent.get_ACT()
+            reward = bandit.play(ACT)
+            agent.update(ACT, reward)
+        agent_Q.append( agent.get_Q() )
+        print(f"\rbatch : {batch_step+1}", end="")
     print("\n", end ="" )
-
-    arm_meanQ = np.mean(arm_batchQ, axis=0)
-    top_rate("agent", arm_meanQ,10)
+    # need to Parallelization : <--    
+    
+    print("[ agent ( step :", steps,", batch :", batch,") ]\n", end = "")
+    agent_meanQ = np.mean(agent_Q, axis=0)
+    util_top(agent_meanQ, 10)
 
     return
 
